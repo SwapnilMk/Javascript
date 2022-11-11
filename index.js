82853361b58e2799e6d03fa17738a4b4f4e5555a
Swapnil Mahadik <mswapnil218@gmail.com>
2022-11-11 14:33:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello world!")
+
+var name = `swapyy`;
+var age = 23;
+
+
+function myDetails(){
+  console.log(name, age)
+}
+
+myDetails()
+// console.log (`my name is ${name} and my age is ${age}`)
