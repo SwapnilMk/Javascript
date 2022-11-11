e51c2348c5cf3bdd72c81ccee061ffc8628134a1
Swapnil Mahadik <mswapnil218@gmail.com>
2022-11-11 15:26:01 +0000

    learning variables :)

---
+++ variables.js
@@ -0,0 +1,63 @@
+console.log("all about variables let var and const")
+
+// let stuName = `swapyy`;
+// let age = 23;
+
+// function myDetails(){
+//   console.log(stuName, age)
+// }
+
+// console.log (`my name is ${stuName} and my age is ${age}`)
+// myDetails()
+
+
+// there are some rules for declare variables 
+// let _name = `swapyy`;
+// let $name = `swapyy`;
+// let name = `swapyy`; // we can do this
+
+// let 123name = `swapyy`;
+// let var = `swapyy`;
+// let @name = `swapyy`;  // we cannot do this
+
+
+
+// redeclaring possible in var 
+// var student = `lokesh`;
+// console.log(student)
+// var student = `swapyy`;
+// console.log(student)
+
+// this is not possible in let and const
+// let student = `lokesh`;
+// console.log(student)
+// let student = `swapyy`;
+// console.log(student)
+
+
+// var has global scope 
+// let and cosnt has a block level scope
+
+// var b = `swapyy`;
+// console.log(b) //swapyy
+// {
+//   var b = `samiyaah`
+//   console.log(b) //samiayaah
+// }
+// console.log(b) // samiyaah
+
+
+
+// let b = `swapyy`;
+// console.log(b) //swapyy
+// {
+//   let b = `samiyaah`
+//   console.log(b) //samiayaah
+// }
+// console.log(b) // swapyy
+
+
+// const name; //cannot do this
+// let name; //do this  //output undefine
+// var name; //do this  //output undefine
+
