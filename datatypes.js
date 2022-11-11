67e8f00b599f07c5c9d2daffb634b888981bd688
Swapnil Mahadik <mswapnil218@gmail.com>
2022-11-11 16:25:36 +0000

    datatypes complete :)

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+// two types of data types 
+// primitive and non primitive
+
+// primitive data types
+// nnbbssu
+let a = 123; //number
+let b = null //null
+let c = true // boolean
+let d = BigInt("1234") //BigInt
+let e = `string` //string
+let f = Symbol("i am symbol") //symbol
+let g = undefined //undefined 
+
+console.log(a,b,c,d,e,f,g)
+
+// non primitive datatypes
+// Object
+
+let obj = {   //this is non primitive datatypes
+  "name" : "swapyy",
+  "surname": "mahadik"
+}
+console.log(obj["name"], obj.surname)
