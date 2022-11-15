2530020f59376d3ec283c19bbc6c1212e3dcda96
SwapnilMk <mswapnil218@gmail.com>
2022-11-15 21:40:56 +0530

    for and while loops!

---
+++ loops.js
@@ -0,0 +1,52 @@
+// for loops
+// for in loop
+// for of loop
+// while loop
+// do while loop
+
+
+// for loop
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+//for in loop
+// let obj = {
+//     1: `swapyy`,
+//     2: `samiyaah`,
+//     3: `lokesh`,
+//     4: `aastha`,
+//     5: `ruchi`,
+//     6: `piyu`
+// }
+
+// console.log(obj)
+
+// for(let names in obj){
+    //     console.log(`the employee ${obj[names]} id is ${names}`)
+    // }
+    
+    
+
+    //for of loop
+    // let arr = [`swap`,`sam`,`lok`,`ass`]
+
+    // for(let elm of arr){
+    //     console.log(elm)
+    // }
+
+
+
+    //while loop
+    // let i = 1
+    // while(i<=10){
+    //     console.log(i)
+    //     i++
+    // }
+
+    // do while loop
+    // let a = 1
+    // do{
+    //     console.log(a)
+    //     a++
+    // }while(a<=10)
