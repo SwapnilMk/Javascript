398745a8d5f6ac96387f3e2cbb7b21c33c1605b4
SwapnilMk <mswapnil218@gmail.com>
2022-11-23 23:03:54 +0530

    events handler

---
+++ events.js
@@ -0,0 +1,40 @@
+// DOM events 
+
+// const alr =()=>{
+//     alert(`hello world!`)
+// }
+
+
+// let btn = document.getElementsByClassName(`btn`)[0]
+// console.log(btn);
+
+// btn.onclick = ()=>{
+//     btn.innerText = `click`
+// }
+
+
+
+// add and remove event listners 
+
+
+// let btn = document.getElementsByClassName(`btn`)[0]
+
+
+// let x =function(){
+//     console.log(`hello world!`);
+// } 
+
+// btn.addEventListener(`click`, x)
+// btn.addEventListener(`click`, function(){
+//     console.log(`hello world! 2`);
+// })
+
+// let y = prompt(`enter`)
+
+// if(y == `1`){
+//     btn.removeEventListener(`click`, x)
+// }
+
+
+
+
---
+++ setTimeOut.js
@@ -0,0 +1,25 @@
+// let a = setTimeout(() => {
+//     console.log(`hey`);
+// }, 1000);
+
+// console.log(a)
+// clearTimeout(a)
+
+
+
+// let b = setInterval(() => {
+//     console.log(`hey`);
+// }, 3000);
+
+// console.log(b);
+// clearInterval(b)
+
+
+
+
+// let sum =(a,b)=>{
+//     console.log(a+b)
+// }
+// setTimeout(sum, 1000, 1,2)
+
+
