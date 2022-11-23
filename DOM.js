c75a9ef13b8c60deaeca171f85366dfb643c5a3f
SwapnilMk <mswapnil218@gmail.com>
2022-11-23 14:02:24 +0530

    Dom manipulation !

---
+++ DOM.js
@@ -0,0 +1,88 @@
+// console.log(window)
+// console.log(document)
+// console.log(document.body)
+
+
+
+// document.body.style.background = `red`
+
+
+// location.href = `https://www.google.co.in/` 
+
+
+// document.body;
+// document.title = `notessss`;
+// document.documentElement;
+
+// console.log(document.body.lastChild)
+// console.log(document.body.lastElementChild)
+// console.log(document.body.firstChild)
+// console.log(document.body.firstElementChild)
+// console.log(document.body.childNodes)
+// console.log(document.body.hasChildNodes())
+
+// let a = document.body.childNodes
+// let b = Array.from(a);
+// console.log(b);
+
+// b.forEach((elm) => {console.info(elm)})
+
+// console.log(document.body.childNodes[document.body.childNodes.length - 1]) // get last element of child node
+
+
+
+
+// console.log(document.body.previousElementSibling.nextElementSibling)
+
+// console.log(document.body.firstElementChild.parentElement.parentNode);
+
+
+
+
+
+// seraching DOM 
+// document.getElementById
+// document.getElementsByClassName
+// document.querySelector
+// document.querySelectorAll
+
+
+
+
+// maches closest contain methods 
+
+// let a = document.body.firstElementChild.lastElementChild
+// console.log(a)
+// console.log(a.matches(`#swap`)); //true  //match and retrun true or false
+// console.log(a.closest(`.p`))
+// console.log(a.contains(`.p`))
+
+
+
+
+
+// document.getElementsByTagName(`nav`)[0].style.background = `grey`
+// document.getElementsByTagName(`nav`)[0].style.color = `white`
+
+// document.getElementsByTagName(`ul`)[0].firstElementChild.style.color = `green`
+// document.getElementsByTagName(`ul`)[0].lastElementChild.style.color = `green`
+
+// Array.from(document.getElementsByTagName(`ul`)).forEach((elm)=>{
+//     elm.style.color = `red`
+// })
+
+
+
+// let acc = document.getElementsByClassName(`container`)[0].lastElementChild;
+// acc.setAttribute(`name` , `swap`)
+// console.log(acc)
+
+
+// table base navigation 
+// let table = document.getElementsByClassName(`container`)[1].firstElementChild
+
+// console.log(table.rows);
+
+
+
+
--- console.js
+++ console.js
@@ -3,11 +3,21 @@
 let a;
 a = 12;
 
-console.time()
-console.log(typeof a);
-console.error(typeof a);
-console.warn(typeof a);
-console.assert(a < 12);
-console.timeEnd();
+// console.time(`code`)
+// console.log(typeof a);
+// console.info(`hey`);
+// console.error(typeof a);
+// console.warn(typeof a);
+// console.assert(a < 12);
+// console.timeEnd(`code`);
 // console.clear();
+// console.dir(console);
 
+// let obj = {
+//     name: `swapyy`,
+//     roll: 21,
+//     age: 22,
+//     surname: `mahadik`
+// }
+
+// console.table(obj)
